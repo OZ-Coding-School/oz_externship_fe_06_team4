2976d64844064a42bde8630aa670c3132510ecbe
woojaemin0106 <dnwoals19@naver.com>
2026-01-20 17:53:52 +0900

    fix:main 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 
 async function enableMocking() {
   if (!import.meta.env.DEV) return
